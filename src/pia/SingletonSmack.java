fc9f3ecc164e0d520c6b92d13e4421de1ef7ff1b
kaisky89 <kaisky89@gmail.com>
2013-06-10 11:58:43 +0200

    implemented addSession()

--- src/pia/SingletonSmack.java
+++ src/pia/SingletonSmack.java
@@ -14,8 +14,10 @@ import org.jivesoftware.smackx.pubsub.ConfigureForm;
 import org.jivesoftware.smackx.pubsub.FormType;
 import org.jivesoftware.smackx.pubsub.Item;
 import org.jivesoftware.smackx.pubsub.LeafNode;
+import org.jivesoftware.smackx.pubsub.PayloadItem;
 import org.jivesoftware.smackx.pubsub.PubSubManager;
 import org.jivesoftware.smackx.pubsub.PublishModel;
+import org.jivesoftware.smackx.pubsub.SimplePayload;
 import org.jivesoftware.smackx.pubsub.listener.ItemDeleteListener;
 import org.jivesoftware.smackx.pubsub.listener.ItemEventListener;
 
@@ -35,6 +37,8 @@ public class SingletonSmack implements NotesCommunicator{
     }
     private Connection connection;
     private PubSubManager mgr;
+    private String sessionCollection;
+    private Integer nextSessionId;
 
     private void connect() throws XMPPException {
         connection = new XMPPConnection(
@@ -75,17 +79,6 @@ public class SingletonSmack implements NotesCommunicator{
         mgr.createNode(name, form);
 
         addItem("availableNodes", name);
-    }
-
-    public void addItem(String nodeString, String id) throws XMPPException {
-
-        // Get the node
-        LeafNode leafnode = mgr.getNode(nodeString);
-
-        // Publish an Item with the specified id
-        Item item;
-        item = new Item(id);
-        leafnode.send(item);
     }
 
     void deleteNode(String nodeName) throws XMPPException {
@@ -148,15 +141,30 @@ public class SingletonSmack implements NotesCommunicator{
         }
         try {
             buildUpStructure();
-            throw new XMPPException();
         } catch (XMPPException ex) {
             throw new NotesCommunicatorException("Error while building up structure.", ex);
         }
     }
 
     @Override
-    public Integer addSession(SessionInformation session) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Integer addSession(SessionInformation session) throws NotesCommunicatorException {
+        
+        // Create the node
+        ConfigureForm form = new ConfigureForm(FormType.submit);
+        form.setAccessModel(AccessModel.open);
+        form.setDeliverPayloads(true);
+        form.setNotifyRetract(true);
+        form.setPersistentItems(true);
+        form.setPublishModel(PublishModel.open);
+        try {
+            mgr.createNode(createSessionId(session), form);
+            addItem(sessionCollection, session);
+        } catch (XMPPException ex) {
+            throw new NotesCommunicatorException(
+                    "cannot create Node for " + session.getName(), ex);
+        }
+        
+        return session.getId();
     }
 
     @Override
@@ -203,6 +211,16 @@ public class SingletonSmack implements NotesCommunicator{
     public void deleteNote(Integer id) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
+    
+    @Override
+    public void lockNote(Integer id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void unlockNote(Integer id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 
     @Override
     public void setAvailableSessionListener(NotesCommunicatorListener<SessionInformation> availableSessionListener) {
@@ -224,7 +242,61 @@ public class SingletonSmack implements NotesCommunicator{
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    private void buildUpStructure() {
+    private void buildUpStructure() throws XMPPException {
+        sessionCollection = "general:allSessions";
+        try {
+            // get node for the collection of all sessions
+            LeafNode node = mgr.getNode(sessionCollection);
+            
+            // get the id of the session with the highest id
+            for (Item item : node.getItems()) {
+                Integer integer = new Integer(item.getId().split(":")[1]);
+                if(integer.compareTo(nextSessionId) > 0)
+                    nextSessionId = integer;
+            }
+
+        } catch (XMPPException ex) {
+            // If Node doesn't exist, create the node
+            LeafNode leaf = mgr.createNode(sessionCollection);
+            ConfigureForm form = new ConfigureForm(FormType.submit);
+            form.setAccessModel(AccessModel.open);
+            form.setDeliverPayloads(true);
+            form.setNotifyRetract(true);
+            form.setPersistentItems(true);
+            form.setPublishModel(PublishModel.open);
+            leaf.sendConfigurationForm(form);
+            nextSessionId = 0;
+        }
+    }
+
+    private String createSessionId(SessionInformation session) {
+        session.setId(++nextSessionId);
+        return session.getId().toString();
+    }
+
+    private void addItem(String nodeString, SessionInformation session) throws XMPPException {
+        // Get the node
+        LeafNode leafnode = mgr.getNode(nodeString);
         
+        // Prepare the payload
+        SimplePayload payload;
+        payload = new SimplePayload("session", "", session.toXML());
+        
+        // Prepare the item
+        PayloadItem item = new PayloadItem("session:" + session.getId(), payload);
+        
+        // send item to the node
+        leafnode.send(item);
+    }
+    
+    private void addItem(String nodeString, String id) throws XMPPException {
+
+        // Get the node
+        LeafNode leafnode = mgr.getNode(nodeString);
+
+        // Publish an Item with the specified id
+        Item item;
+        item = new Item(id);
+        leafnode.send(item);
     }
 }
