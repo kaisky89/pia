180a5b3f255951682f0c6fe8714093d5d57c120e
kaisky89 <kaisky89@gmail.com>
2013-06-13 14:56:31 +0200

    added the attribute "locked" to NoteInformation Class

--- src/pia/NoteInformation.java
+++ src/pia/NoteInformation.java
@@ -4,15 +4,18 @@
  */
 package pia;
 
+import java.io.IOException;
 import java.io.StringReader;
 import java.util.HashMap;
 import java.util.Map;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
 /**
  *
@@ -27,7 +30,7 @@ public abstract class NoteInformation {
             DocumentBuilder builder = factory.newDocumentBuilder();
             StringReader sr = new StringReader(xml);
             document = builder.parse(new InputSource(sr));
-        } catch (Exception ex) {
+        } catch (ParserConfigurationException | SAXException | IOException ex) {
             throw new NotesCommunicatorException("Error while parsing xml: " + xml, ex);
         }
 
