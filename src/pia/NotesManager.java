3e3997f1dd2682da5161601681868356426a639a
kaisky89 <kaisky89@gmail.com>
2013-06-19 16:54:30 +0200

    started implementing NotesManager

---
+++ src/pia/NotesManager.java
@@ -0,0 +1,19 @@
+package pia;
+
+/**
+ * This Class acts as a persistent module to handle requests and events between
+ * the View and the NotesCommunicator.
+ * User: kaisky89
+ * Date: 19.06.13
+ * Time: 15:37
+ */
+public class NotesManager {
+
+    private NotesCommunicator communicator = SingletonSmack.getInstance();
+
+    public NotesManager(){
+        // TODO: ConstructorCode
+    }
+
+    //public
+}
